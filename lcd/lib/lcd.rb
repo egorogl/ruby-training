32a7b5f84dd4766d5c5a5c9da86ef5f91ae05f9d
egorogl <egorogl@gmail.com>
2022-09-06 13:46:04 +0500

    Passed

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -1,5 +1,39 @@
-# frozen_string_literal: true
-
 class LCD
-  # Write your code in here
+  ALPHA = {
+    a: [' _ ', '|_|', '| |'],
+    b: ['   ', '|_ ', '|_|'],
+    c: [' _ ', '|  ', '|_ '],
+    d: ['   ', ' _|', '|_|'],
+    e: [' _ ', '|_ ', '|_ '],
+    f: [' _ ', '|_ ', '|  '],
+    '0': [' _ ', '| |', '|_|'],
+    '1': ['   ', '  |', '  |'],
+    '2': [' _ ', ' _|', '|_ '],
+    '3': [' _ ', ' _|', ' _|'],
+    '4': ['   ', '|_|', '  |'],
+    '5': [' _ ', '|_ ', ' _|'],
+    '6': [' _ ', '|_ ', '|_|'],
+    '7': [' _ ', '  |', '  |'],
+    '8': [' _ ', '|_|', '|_|'],
+    '9': [' _ ', '|_|', ' _|']
+  }
+
+  def initialize(str, options = {})
+    @str = str.to_s
+    @width = options[:width] || 1
+    @height = options[:height] || 1
+  end
+
+  def render
+    ret = ''
+    
+    3.times do |x|
+      @str.each_char do |ch|
+        s = ALPHA[ch.to_sym][x]
+        ret += s[0] + (s[1] * @width) + s[2]
+      end
+      ret += "\n"
+    end
+    ret
+  end
 end
